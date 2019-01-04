86de00d0ce9a96db7ba93db2bc758830c16c42d0
RockettSally <ryusan357@gmail.com>
2019-01-03 22:07:00 -0200

    Aula 9 - Configuração do Mongoose (MongoDB)

--- bin/configurations/variables.js
+++ bin/configurations/variables.js
@@ -1,6 +1,9 @@
 const variables = {
     Api: {
         port: process.env.port || 3000
+    },
+    Database: {
+        connection: process.env.connection || 'mongodb://admin:no-food225@ds149034.mlab.com:49034/nofood_dev'
     }
 }
 
--- bin/express.js
+++ bin/express.js
@@ -3,6 +3,8 @@
  */
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const variables = require('./configurations/variables');
 
 /**
  * ROUTERS
@@ -13,6 +15,11 @@ const routes = require('../routes/routes');
  * WEB SERVER INVOKE
  */
 const app = express();
+
+/**
+ * MONGODB CONNECTION - MONGOOSE
+ */
+mongoose.connect(variables.Database.connection);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
