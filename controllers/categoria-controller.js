fb4a536637a053d51ffd60b62caa0529127b8ed5
RockettSally <ryusan357@gmail.com>
2019-01-03 23:02:57 -0200

    Aula 10 -> Criando Models e DB

--- controllers/categoria-controller.js
+++ controllers/categoria-controller.js
@@ -1,27 +1,37 @@
 'use strict'
+require('../models/categoria-model');
+const mongoose = require('mongoose');
+const categoria = mongoose.model('Categoria');
 
 function categoriaController(){
 
 }
 
 categoriaController.prototype.get = async (req, res) => {
-    res.status(200).send('Hello categoria-router');
+    let lista = await categoria.find();
+    res.status(200).send(lista);
 };
 
 categoriaController.prototype.getById = async (req, res) => {
-    res.status(200).send(`ID: ${req.params.id}`);
+    let categoriaEncontrada = await categoria.findById(req.params.id);
+    res.status(200).send(categoriaEncontrada);
 };
 
 categoriaController.prototype.post = async (req, res) => {
-    
+    let novaCategoria = new categoria(req.body);
+    let resultado = await novaCategoria.save();
+    res.status(201).send(resultado);
 };
 
 categoriaController.prototype.put = async (req, res) => {
-
+    await categoria.findByIdAndUpdate(req.params.id, { $set: req.body });
+    let categoriaEncontrada = await categoria.findById(req.params.id);
+    res.status(202).send(categoriaEncontrada);
 };
 
 categoriaController.prototype.delete = async (req, res) => {
-
+    let itemRemovido = await categoria.findByIdAndDelete(req.params.id);
+    res.status(204).send(itemRemovido);
 };
 
 module.exports = categoriaController;
--- controllers/produto-controller.js
+++ controllers/produto-controller.js
@@ -1,27 +1,37 @@
 'use strict'
+require('../models/produto-model');
+const mongoose = require('mongoose');
+const produto = mongoose.model('Produto');
 
 function produtoController(){
 
 }
 
 produtoController.prototype.get = async (req, res) => {
-    res.status(200).send('Hello produto-router');
+    let lista = await produto.find();
+    res.status(200).send(lista);
 };
 
 produtoController.prototype.getById = async (req, res) => {
-    res.status(200).send(`ID: ${req.params.id}`);
+    let produtoEncontrado = await produto.findById(req.params.id);
+    res.status(200).send(produtoEncontrado);
 };
 
 produtoController.prototype.post = async (req, res) => {
-    
+    let novoProduto = new produto(req.body);
+    let resultado = await novoProduto.save();
+    res.status(201).send(resultado);
 };
 
 produtoController.prototype.put = async (req, res) => {
-
+    await produto.findByIdAndUpdate(req.params.id, { $set: req.body });
+    let produtoEncontrado = await produto.findById(req.params.id);
+    res.status(202).send(produtoEncontrado);
 };
 
 produtoController.prototype.delete = async (req, res) => {
-
+    let itemRemovido = await produto.findByIdAndDelete(req.params.id);
+    res.status(204).send(itemRemovido);
 };
 
 module.exports = produtoController;
